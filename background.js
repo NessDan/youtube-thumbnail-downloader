37da0727d545eb330d6d5669f7ce5a47b46f39cd
NessDan <NessDan@users.noreply.github.com>
2023-09-26 01:28:51 -0400

    Fix where download thumbnail context menu shows up

--- background.js
+++ background.js
@@ -2,15 +2,24 @@ import { parseVideoId, downloadThumbnail } from "./download-helpers.js";
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
-    id: "downloadThumbnail",
+    id: "downloadThumbnailLink",
     title: "Download thumbnail",
-    contexts: ["page", "link"],
+    contexts: ["link"],
     targetUrlPatterns: ["*://www.youtube.com/watch?v=*", "*://youtu.be/*"],
+  });
+  chrome.contextMenus.create({
+    id: "downloadThumbnailPage",
+    title: "Download thumbnail",
+    contexts: ["page"],
+    documentUrlPatterns: ["*://www.youtube.com/watch?v=*", "*://youtu.be/*"],
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "downloadThumbnail") {
+  if (
+    info.menuItemId === "downloadThumbnailLink" ||
+    info.menuItemId === "downloadThumbnailPage"
+  ) {
     const videoId = parseVideoId(info.linkUrl || tab.url);
     downloadThumbnail(videoId);
   }
